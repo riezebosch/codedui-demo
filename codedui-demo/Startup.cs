a8fba346e08e826e8d0c08ae5b076df4fae77eb6
Manuel Riezebosch <mriezebosch@gmail.com>
2016-02-11 12:46:55 +0100

    nieuwe MVC solution met tests

---
+++ codedui-demo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(codedui_demo.Startup))]
+namespace codedui_demo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
