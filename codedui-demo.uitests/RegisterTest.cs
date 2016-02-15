c55f158e4c54f78af3b375f1d16f71f42ed900b4
Manuel Riezebosch <mriezebosch@gmail.com>
2016-02-15 14:28:05 +0100

    playback speedup by tweaking settings

--- codedui-demo.uitests/RegisterTest.cs
+++ codedui-demo.uitests/RegisterTest.cs
@@ -1,13 +1,8 @@
-﻿using codedui_demo.uitests.Account;
-//using Microsoft.Services.TestTools.UITesting.Html;
-using Microsoft.VisualStudio.TestTools.UITesting;
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Shouldly;
 using codedui_demo.uitests.Home;
 using Microsoft.VisualStudio.TestTools.UnitTesting.Web;
@@ -18,43 +13,36 @@ namespace codedui_demo.uitests
     [CodedUITest]
     public class RegisterTest
     {
-        public TestContext TestContext { get; set; }
+        public TestContext TestContext
+        {
+            get;
+            set;
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            SpeedUp.Setup();
+        }
 
         [TestMethod]
         [AspNetDevelopmentServer("web", "codedui-demo")]
         public void Register()
         {
-            
             var home = Page.Launch<HomePage>(FromAspNetDevelopmentServer());
-            var register = home
-                .ClickRegister();
-
+            var register = home.ClickRegister();
             var email = CreateUniqueEmail();
-            register.EnterEmail(email)
-                .EnterPassword("P@ssw0rd")
-                .EnterConfirmPassword("P@ssw0rd")
-                .ClickRegister()
-                .IsLoggedIn(email)
-                .ShouldBeTrue();
+            register.EnterEmail(email).EnterPassword("P@ssw0rd").EnterConfirmPassword("P@ssw0rd").ClickRegister().IsLoggedIn(email).ShouldBeTrue();
         }
-
 
         [TestMethod]
         [AspNetDevelopmentServer("web", "codedui-demo")]
         public void RegisterAndLogOff()
         {
             var home = Page.Launch<HomePage>(FromAspNetDevelopmentServer());
-            var register = home
-                .ClickRegister();
-
+            var register = home.ClickRegister();
             var email = CreateUniqueEmail();
-            register.EnterEmail(email)
-                .EnterPassword("P@ssw0rd")
-                .EnterConfirmPassword("P@ssw0rd")
-                .ClickRegister()
-                .Logoff()
-                .IsLoggedIn()
-                .ShouldBeFalse();
+            register.EnterEmail(email).EnterPassword("P@ssw0rd").EnterConfirmPassword("P@ssw0rd").ClickRegister().Logoff().IsLoggedIn().ShouldBeFalse();
         }
 
         [TestMethod]
@@ -62,27 +50,16 @@ namespace codedui_demo.uitests
         public void TestWeakPassword()
         {
             var home = Page.Launch<HomePage>(FromAspNetDevelopmentServer());
-            var register = home
-                .ClickRegister();
-
+            var register = home.ClickRegister();
             var email = CreateUniqueEmail();
-            register.EnterEmail(email)
-                .EnterPassword("password")
-                .EnterConfirmPassword("password")
-                .ClickRegister();
-
-            register
-                .Errors
-                .Any(m => m.Contains("Passwords must have"))
-                .ShouldBeTrue();
-
+            register.EnterEmail(email).EnterPassword("password").EnterConfirmPassword("password").ClickRegister();
+            register.Errors.Any(m => m.Contains("Passwords must have")).ShouldBeTrue();
         }
 
         private Uri FromAspNetDevelopmentServer()
         {
             var stack = new StackTrace();
             var attr = stack.GetFrame(1).GetMethod().GetCustomAttributes(true).OfType<AspNetDevelopmentServerAttribute>().FirstOrDefault();
-
             if (attr == null)
             {
                 throw new InvalidOperationException("This method may only be called directly from the test method and that method should have the AspNetDevelopmentServer attribute.");
---
+++ codedui-demo.uitests/SpeedUp.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+
+namespace codedui_demo.uitests
+{
+    /// <summary>
+    /// Thanks to http://stackoverflow.com/a/20406849/129269 for some playback settings tweaks for speedup.
+    /// </summary>
+    static class SpeedUp
+    {
+        /// <summary>
+        /// Invoke this method from the TestInitialize because it only then affects the playback.
+        /// </summary>
+        public static void Setup()
+        {
+            // Configure the playback engine
+            Playback.PlaybackSettings.WaitForReadyLevel = Microsoft.VisualStudio.TestTools.UITest.Extension.WaitForReadyLevel.Disabled;
+            Playback.PlaybackSettings.MaximumRetryCount = 10;
+            Playback.PlaybackSettings.ShouldSearchFailFast = false;
+            Playback.PlaybackSettings.DelayBetweenActions = 500;
+            Playback.PlaybackSettings.SearchTimeout = 1000;
+            // Add the error handler
+            Playback.PlaybackError -= Playback_PlaybackError; // Remove the handler if it's already added
+            Playback.PlaybackError += Playback_PlaybackError; // Ta dah...
+        }
+
+        /// <summary> PlaybackError event handler. </summary>
+        private static void Playback_PlaybackError(object sender, PlaybackErrorEventArgs e)
+        {
+            // Wait a second
+            System.Threading.Thread.Sleep(1000);
+            // Retry the failed test operation
+            e.Result = PlaybackErrorOptions.Retry;
+        }
+    }
+}
