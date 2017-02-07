2139fef5e106252aa5e5f62e70131db6c21907d4
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-07 16:49:22 +0100

    test inheritance voor uitvoerern tegen IE en Chrome en phantomjs

---
+++ codedui-demo.uitests/RegisterTest.IE.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+
+namespace codedui_demo.uitests
+{
+    [TestClass]
+    [DeploymentItem(@"IEDriverServer.exe")]
+    public class RegisterTestIE : RegisterTest
+    {
+        protected override IWebDriver InitializeDriver()
+        {
+            return new InternetExplorerDriver();
+        }
+    }
+}
---
+++ codedui-demo.uitests/RegisterTest.PhantomJS.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.PhantomJS;
+
+namespace codedui_demo.uitests
+{
+    [TestClass]
+    [DeploymentItem(@"phantomjs.exe")]
+    public class RegisterTestPhantomJS : RegisterTest
+    {
+        protected override IWebDriver InitializeDriver()
+        {
+            var driver = new PhantomJSDriver();
+            driver.Manage().Window.Size = new System.Drawing.Size(1920, 1080);
+
+            return driver;
+        }
+    }
+}
