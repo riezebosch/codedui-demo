e4b1ce626394bd6c718f44773b585f492ac3dee4
Manuel Riezebosch <mriezebosch@gmail.com>
2017-02-09 10:04:54 +0100

    page object pattern for winforms

--- codedui-winforms.uitests/CodedUITest1.cs
+++ codedui-winforms.uitests/CodedUITest1.cs
@@ -8,7 +8,8 @@ using Microsoft.VisualStudio.TestTools.UITesting;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Microsoft.VisualStudio.TestTools.UITest.Extension;
 using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
-
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+using Shouldly;
 
 namespace codedui_winforms.uitests
 {
@@ -19,71 +20,15 @@ namespace codedui_winforms.uitests
     [DeploymentItem("codedui-winforms.exe")]
     public class CodedUITest1
     {
-        public CodedUITest1()
-        {
-        }
-
         [TestMethod]
         public void CodedUITestMethod1()
         {
-            // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-            ApplicationUnderTest.Launch("codedui-winforms.exe");
-            this.UIMap.EnterNumber();
-            this.UIMap.AssertOutput();
-
-
+            var page = new MainPage();
+            page.Start()
+                .EnterInput(5)
+                .ClickButton()
+                .ReadOutput()
+                .ShouldBe("5");
         }
-
-        #region Additional test attributes
-
-        // You can use the following additional attributes as you write your tests:
-
-        ////Use TestInitialize to run code before running each test 
-        //[TestInitialize()]
-        //public void MyTestInitialize()
-        //{        
-        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-        //}
-
-        ////Use TestCleanup to run code after each test has run
-        //[TestCleanup()]
-        //public void MyTestCleanup()
-        //{        
-        //    // To generate code for this test, select "Generate Code for Coded UI Test" from the shortcut menu and select one of the menu items.
-        //}
-
-        #endregion
-
-        /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
-        public TestContext TestContext
-        {
-            get
-            {
-                return testContextInstance;
-            }
-            set
-            {
-                testContextInstance = value;
-            }
-        }
-        private TestContext testContextInstance;
-
-        public UIMap UIMap
-        {
-            get
-            {
-                if ((this.map == null))
-                {
-                    this.map = new UIMap();
-                }
-
-                return this.map;
-            }
-        }
-
-        private UIMap map;
     }
 }
---
+++ codedui-winforms.uitests/MainPage.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UITesting;
+using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codedui_winforms.uitests
+{
+    class MainPage
+    {
+        private WinWindow Main { get; set; }
+
+        public MainPage Start()
+        {
+            var app = ApplicationUnderTest.Launch("codedui-winforms.exe");
+            Main = new WinWindow(app);
+            Main.WindowTitles.Add("Form1");
+
+            return this;
+        }
+        public MainPage EnterInput(int input)
+        {
+            var wrapper = new WinWindow(Main);
+            wrapper.SearchProperties[WinButton.PropertyNames.Name] = "textBox1";
+
+            var text = new WinEdit(wrapper);
+            text.Text = input.ToString();
+
+            return this;
+        }
+
+        public MainPage ClickButton()
+        {
+            var wrapper = new WinWindow(Main);
+            wrapper.SearchProperties[WinButton.PropertyNames.Name] = "button1";
+
+            var button = new WinButton(wrapper);
+            Mouse.Click(button);
+
+            return this;
+        }
+
+     
+        public string ReadOutput()
+        {
+            var wrapper = new WinWindow(Main);
+            wrapper.SearchProperties[WinButton.PropertyNames.Name] = "label1";
+
+            var label = new WinText(wrapper);
+            return label.DisplayText;
+        }
+    }
+}
